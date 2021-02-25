4e585bf4b91c0a7a81c39f3526406820c2ad384b
Maria Karanasou <karanasou@gmail.com>
2021-02-25 16:28:13 +0200

    side modules: component management, pipelines

---
+++ front-end/src/app/safe-html.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safeHtml'
+})
+export class SafeHtmlPipe implements PipeTransform {
+
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+
+  transform(value: string): SafeHtml {
+    return this.domSanitizer.bypassSecurityTrustHtml(value);
+  }
+}
