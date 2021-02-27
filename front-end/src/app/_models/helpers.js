c0048e304fe436cb7a4492c7f4ee49ad658d901e
Maria Karanasou <karanasou@gmail.com>
2021-02-27 19:01:37 +0200

    Revert "Issue 5 cleanup"

--- front-end/src/app/_models/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var environment_1 = require("../../environments/environment");
-function validateFileSize(file) {
-    return sizeInMB(file.size) <= environment_1.environment.maxFileSize;
-}
-function sizeInMB(sizeInBytes) {
-    return sizeInBytes / 1024 / 1024;
-}
--- front-end/src/app/_models/helpers.ts
+++
@@ -1,9 +0,0 @@
-import {environment} from '../../environments/environment';
-
-export function validFileSize(file: File): boolean {
-  return sizeInMB(file.size) <= environment.maxFileSize;
-}
-
-function sizeInMB(sizeInBytes): number{
-  return sizeInBytes / 1024 / 1024;
-}
