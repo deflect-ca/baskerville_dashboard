0192abcc1add62a56559e85a81a053e552c75a06
Maria Karanasou <karanasou@gmail.com>
2021-03-11 17:04:48 +0200

    fixing env variables and node types

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseApiUrl: '${process.env.API_BASE_URL}/api/1',
-  socketUrl: '${process.env.API_BASE_URL}',
+  baseApiUrl: `${process.env.API_BASE_URL}/api/1`,
+  socketUrl: `${process.env.API_BASE_URL}`,
   maxFileSize: 50,
   submitMethod: 'local'
 };
