2aceafcdf2ca655f4b422760848685d2fc543b66
Maria Karanasou <karanasou@gmail.com>
2021-03-11 13:08:15 +0200

    ng build --prod changes

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  baseApiUrl: 'http://127.0.0.1:5000/api/1',
+  socketUrl: 'http://127.0.0.1:5000',
+  maxFileSize: 50,
+  submitMethod: 'local'
 };
 
